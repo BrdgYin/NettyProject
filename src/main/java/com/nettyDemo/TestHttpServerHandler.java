9204f947e08d598f344bb62a5c5f2b93ae879133
casen330 <kaifengxu_m.e@foxmail.com>
2021-03-06 20:57:05 +0800

    p6

--- src/main/java/com/nettyDemo/TestHttpServerHandler.java
+++ src/main/java/com/nettyDemo/TestHttpServerHandler.java
@@ -1,12 +1,16 @@
 package com.nettyDemo;
 
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
 import io.netty.buffer.ByteBuf;
+
+import java.net.URI;
 
 // 自定义的处理器
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
@@ -16,7 +20,25 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     // 5.0中改为messageRecevied()
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+        // 获取当前连接的httpObject类型
+        System.out.println("当前msg:" + httpObject.getClass().getSimpleName());
+        // 获取远程连接的的地址
+        System.out.println("当前客户端的地址:" + channelHandlerContext.channel().remoteAddress());
+
         if (httpObject instanceof HttpRequest) {
+            // 强制类型转换
+            HttpRequest httpRequest = (HttpRequest) httpObject;
+
+            /*// curl -X POST[PUT] "-----"
+            // 获取请求的方法名
+            System.out.println("方法名：" + httpRequest.method().name());
+            // 方法名：GET
+
+            // 获取请求的uri
+            URI url = new URI(httpRequest.uri());
+            System.out.println(url);
+            // kkk/*/
+
             // 向客户端发送的数据
             ByteBuf content = Unpooled.copiedBuffer("hello World", CharsetUtil.UTF_8);
 
@@ -29,5 +51,53 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
             channelHandlerContext.writeAndFlush(response);
         }
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved");
+        super.handlerRemoved(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelReadComplete");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelWritabilityChanged");
+        super.channelWritabilityChanged(ctx);
     }
 }
